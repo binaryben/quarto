aeacc494a0e369954a83cf1f4c8fe12f1fc1174e
Benny Michaels <12827556+binaryben@users.noreply.github.com>
2023-03-24 15:21:55 +0800

    🎉 New svelte widget

---
+++ geewiz/widgets/controller/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript')
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	},
+	ignorePatterns: [
+		'*.cjs',
+		'.DS_Store',
+		'node_modules',
+		'/build',
+		'/.svelte-kit',
+		'/package',
+		'.env',
+		'.env.*',
+		'!.env.example',
+		'pnpm-lock.yaml',
+		'package-lock.json',
+		'yarn.lock'
+	]
+};
---
+++ geewiz/widgets/controller/README.md
@@ -0,0 +1,1 @@
+# Virtual Masso Controller Widget
